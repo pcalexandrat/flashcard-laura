698be4d82851a86c06edd80fa30e80793eab265f
lauracorrea08 <141340457+lauracorrea08@users.noreply.github.com>
2024-11-11 09:09:35 -0300

    .

--- app.js
+++ app.js
@@ -13,3 +13,17 @@ function criaCartao(categoria, pergunta, resposta) {
         <p>${resposta}</p>
     </div>
     </div>
+    `
+
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', viraCartao)
+
+
+    container.appendChild(cartao)
+
+}
